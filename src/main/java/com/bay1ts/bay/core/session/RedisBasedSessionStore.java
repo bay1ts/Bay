4773d1687fc8ff65d165434753bf03990c48dcf5
bay <chenuu0110@gmail.com>
2016-10-18 16:39:17 +0800

    用spring data redis 做了 session的 redis存储

--- src/main/java/com/bay1ts/bay/core/session/RedisBasedSessionStore.java
+++ src/main/java/com/bay1ts/bay/core/session/RedisBasedSessionStore.java
@@ -1,26 +1,68 @@
 package com.bay1ts.bay.core.session;
 
+import com.bay1ts.bay.Config;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+import java.util.Set;
+import java.util.UUID;
+
 /**
  * Created by chenu on 2016/10/15.
  */
-public class RedisBasedSessionStore implements  BaseSessionStore {
+public class RedisBasedSessionStore implements BaseSessionStore {
+    private JedisConnectionFactory factory;
+
+    private RedisTemplate<String, HttpSessionImpl> redisTemplate;
+    RedisBasedSessionStore(){
+        this.factory=new JedisConnectionFactory();
+        this.redisTemplate=new RedisTemplate<>();
+        factory.setDatabase(0);
+        factory.setHostName(Config.getRedisLocate());
+        factory.setPort(Config.getPort());
+        factory.setPassword(Config.getRedisPassword());
+        redisTemplate.setConnectionFactory(factory);
+    }
+
     @Override
     public HttpSessionImpl findSession(String sessionId) {
-        return null;
+        if (sessionId == null)
+            return null;
+        return redisTemplate.opsForValue().get(sessionId);
     }
 
     @Override
     public HttpSessionImpl createSession() {
-        return null;
+        String sessionId = generateNewSessionId();
+        HttpSessionImpl session = new HttpSessionImpl(sessionId);
+        redisTemplate.opsForValue().set(sessionId, session);
+        return session;
     }
 
     @Override
     public void destroySession(String sessionId) {
-
+        redisTemplate.delete(sessionId);
     }
 
     @Override
     public void destroyInactiveSessions() {
+        Set<byte[]> keys = redisTemplate.getConnectionFactory().getConnection().keys("*".getBytes());
+        HttpSessionImpl session = null;
+        for (byte[] key : keys) {
+            session = redisTemplate.opsForValue().get(key);
+            if (session.getMaxInactiveInterval() < 0)
+                continue;
+            long currentMillis = System.currentTimeMillis();
 
+            if (currentMillis - session.getLastAccessedTime() > session
+                    .getMaxInactiveInterval() * 1000) {
+                destroySession(new String(key));
+            }
+
+        }
+    }
+
+    protected String generateNewSessionId() {
+        return UUID.randomUUID().toString();
     }
 }
